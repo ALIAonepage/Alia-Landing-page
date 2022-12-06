a7f9764f7bd2a977780ad3026aeba449c73cb41b
liviabion <llb@cin.ufpe.br>
2022-12-06 19:06:18 -0300

    feature(feature/backendPartners): get, delete and update methods

--- server/src/controllers/PartnersController.ts
+++ server/src/controllers/PartnersController.ts
@@ -16,5 +16,33 @@ export default class PartnersController implements Crud {
         return response.status(httpStatus).send({ message });
     }
 
+    async get(request: Request, response: Response){
+        const {httpStatus, values} = await Citi.getAll(Partners);
+        return response.status(httpStatus).send(values);
+    }
+
+    async delete(request: Request, response: Response){
+        const { id } = request.params;
+        const {value: partnersFound, message } = await Citi.findByID(Partners, id); 
+           
+        if(!partnersFound) return response.status(400).send({ message });
+
+        const {httpStatus, messageFromDelete } = await Citi.deleteValue(Partners, partnersFound);
+        return response.status(httpStatus).send({ messageFromDelete });
+    }
+
+    async update(request: Request, response: Response){
+        const { id } = request.params;
+        const {imagePartner1, imagePartner2} = request.body;
+
+        const isAnyUndefined = Citi.areValuesUndefined(imagePartner1, imagePartner2, id);
+        if(isAnyUndefined) return response.status(400).send();
+
+        const partnersUpdated = {imagePartner1, imagePartner2};
+
+        const { httpStatus, messageFromUpdate } = await Citi.updateValue(Partners, id, partnersUpdated);
+        return response.status(httpStatus).send({ messageFromUpdate });
+    }
+
     
 }

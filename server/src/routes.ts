058ba370e9b803a8a9ce75828306e49a482f3dcd
liviabion <llb@cin.ufpe.br>
2022-12-06 20:16:37 -0300

    feature(feature/backendSubscription): Subscription model and create, get, update, delete methods

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import SubscriptionController from '@controllers/SubscriptionController'
 
 const routes = express.Router();
 const userController = new UserController();
 
+routes.post('/user', userController.create);
+routes.get('/user', userController.get);
+routes.delete('/user/:id', userController.delete);
+routes.put('/user/:id', userController.update);
+
+const subscriptionController = new SubscriptionController();
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);

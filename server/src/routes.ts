cde93749e01fd65899ed3f2813c049bbdc49dd13
ricardobizerra <rblf@cin.ufpe.br>
2022-12-06 21:14:27 -0300

    hotfix(feature/backendSubscription): adjusting Subscription routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,10 +11,10 @@ routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
 const subscriptionController = new SubscriptionController();
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
+routes.post('/subscription', subscriptionController.create);
+routes.get('/subscription', subscriptionController.get);
+routes.delete('/subscription/:id', subscriptionController.delete);
+routes.put('/subscription/:id', subscriptionController.update);
 
 
 export default routes;

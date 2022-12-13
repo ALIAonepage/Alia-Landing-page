1abac65d5bf6028396df8be63ca1b36c3399a7f3
ricardobizerra <rblf@cin.ufpe.br>
2022-12-12 21:33:15 -0300

    hotfix(feature/frontSubscription): refactoring home index page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,30 +1,10 @@
 import React from "react";
 
-import { HomeContainer } from './styles';
 import { SubscriptionComponent } from "../../components";
-
-import { LogoCITi } from '../../assets';
 
 
 export const Home: React.FC = () => {
     return (
-        <div>
-            <HomeContainer>
-                <img src={LogoCITi} alt="" />
-                <h1>ReactJS Boilerplate</h1>
-                <p>
-                    Made with
-                    {' '}
-                    <strong>&lt; &#x0002F; &gt;</strong>
-                    {' '}
-                    and
-                    {' '}
-                    <strong>&hearts;</strong>
-                    {' '}
-                    by CITi
-                </p>
-            </HomeContainer>
-            <SubscriptionComponent />
-        </div>
+        <SubscriptionComponent />
     );
 }

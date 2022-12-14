463c4eab0bc08e2597a9b82e4f985bf64f324f78
liviabion <llb@cin.ufpe.br>
2022-12-13 23:42:59 -0300

    feature(feature/frontIntroduction): Introduction Component

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,13 @@
-import LogoCITi from './logo.png';
+import AliaLogo from './aliaLogo.png';
+import ImgIntroduction from './img.png';
+import PoppinsRegular from './noir-pro/Poppins-Bold.ttf';
+import PoppinsBold from './noir-pro/Poppins-ExtraBold.ttf';
+import PoppinsSlim from './noir-pro/Poppins-Regular.ttf';
 
 export {
-    LogoCITi
+    AliaLogo,
+    ImgIntroduction,
+    PoppinsSlim,
+    PoppinsBold,
+    PoppinsRegular
 };
---
+++ client/src/components/index.ts
@@ -0,0 +1,5 @@
+import IntroductionComponent from "./Introduction";
+
+export {
+    IntroductionComponent
+}

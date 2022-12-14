727e44bee9c42dea1d9303a8417cd81563394284
liviabion <llb@cin.ufpe.br>
2022-12-14 17:17:49 -0300

    feature(feature/integrationStages): get methods, apiaxios

--- client/src/components/Stages/index.tsx
+++ client/src/components/Stages/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { StagesContainer, StyledStages, ContainerTag, Components, StagesIcon, StagesTitle} from "./styles";
 import { RecStages } from "../../assets";
+import axios from "axios";
+import apiAxios from "../../services/api";
 
 type StagesProps = {
     icon: string;
@@ -8,6 +11,7 @@ type StagesProps = {
 }
 
 export const StagesTag = ({icon, title}: StagesProps) => {
+    
     return(
         <ContainerTag>
             <StagesIcon>
@@ -20,16 +24,27 @@ export const StagesTag = ({icon, title}: StagesProps) => {
 }
 
 export const StagesComponent: React.FC = () => {
+
+    const [icon, setIcon] = useState('');
+    const [title, setTitle] = useState('');
+
+    const handleGet = async () => {
+        await apiAxios.get('/steps')
+         .then(res => {console.log(res)})
+         .catch(err => {console.log(err)})
+    
+    }
+
     return(
         <StyledStages>
             <StagesContainer>
                 <h1>Etapas</h1>
                 <img id="RecStages" src={RecStages}/>
                 <Components>
-                    <StagesTag icon={"img"} title={"texto"}/>
-                    <StagesTag icon="img" title="texto"/>
-                    <StagesTag icon="img" title="texto"/>
-                    <StagesTag icon="img" title="texto"/>
+                    <StagesTag icon={icon} title={title}/>
+                    <StagesTag icon={icon} title={title}/>
+                    <StagesTag icon={icon} title={title}/>
+                    <StagesTag icon={icon} title={title}/>
                 </Components>
           </StagesContainer>
         </StyledStages>

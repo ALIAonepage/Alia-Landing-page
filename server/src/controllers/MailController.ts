2479c56aab4e27bad96965996b8fde9bed8446f3
ricardobizerra <rblf@cin.ufpe.br>
2022-12-13 23:58:54 -0300

    feature(feature/SubscriptionIntegration): nodemailer integration

---
+++ server/src/controllers/MailController.ts
@@ -0,0 +1,52 @@
+import { MailServer } from "src/services/MailServer";
+import dotenv from "dotenv";
+import { Request, Response } from "express";
+
+dotenv.config();
+
+const SendMail = async (request: Request, response: Response) => {
+
+    try {
+        const { firstName, 
+            phoneNumber, 
+            email, 
+            company, 
+            partnersNumber, 
+            segment, 
+            solutionDescribe,
+        site } = request.body;
+
+        await MailServer({
+            destinationUser: process.env.EMAIL_WILL_RECEIVE,
+            subjectText: 'nodemailer funfando',
+            htmlOption: `<p>
+                Um novo cadastro foi realizado!
+                
+                Estas foram as informações enviadas:
+
+                Nome: ${firstName}
+                Número de telefone: ${phoneNumber}
+                E-mail: ${email}
+                Empresa: ${company}
+                Número de sócios: ${partnersNumber}
+                Segmento: ${segment}
+                Descrição da solução: ${solutionDescribe}
+                Site: ${site}
+            </p>`
+        })
+
+        return response.status(200).send({
+            answer: "Enviado."
+        })
+
+    } catch (error) {
+        return response.status(500).send({
+            answer: "Desculpe, houve um problema."
+        })
+    }
+
+}
+
+export {
+    SendMail
+}

34a215c6f90fc32183d4663e62862e5a8733e70c
liviabion <llb@cin.ufpe.br>
2022-12-18 13:53:18 -0300

    hotfix(develop): introduction styles)

--- client/src/components/Introduction/index.tsx
+++ client/src/components/Introduction/index.tsx
@@ -6,13 +6,11 @@ import { AliaLogo } from "../../assets";
 export const IntroductionComponent: React.FC = () => {
     return (
         <IntroductionContainer>
-            <div id="color">
-                <img src={AliaLogo} />
-                <div id="text">
-                    <h1>Venha potencializar <br/> seu serviço!</h1>
-                    <h2>Faça parte do nosso ecossistema <br/> de empresas e startups </h2>
-                    <a href="#Forms"> Inscrição</a>
-                </div>
+            <img src={AliaLogo} />
+            <div id="text">
+                <h1>Venha potencializar <br/> seu serviço!</h1>
+                <h2>Faça parte do nosso ecossistema <br/> de empresas e startups </h2>
+                <a href="#Forms"> Inscrição</a>
             </div>
         </IntroductionContainer>
     )
